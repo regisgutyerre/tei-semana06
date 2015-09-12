d49edf4036cb14dcdc8f738e2779b45dbd1c2194
regis <regisgutyerre@hotmail.com>
2015-09-12 10:39:56 -0300

    primeiro commit

---
+++ contato.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Contato</title>
+</head>
+<body>
+	<a href="index.php">Ir para Index</a>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Index</title>
+</head>
+<body>
+	<a href="contato.php">Ir para Contato</a>
+</body>
+</html>
